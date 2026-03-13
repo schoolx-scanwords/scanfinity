7577bfcba5aec3eb9733ed3cd00320aebcbe4fee
Adizs-wq <Andrey04Kras@yandex.ru>
2026-03-14 01:16:36 +0300

    Связь фронта с беком

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from .health import router as health_router
 from .register_user import router as register_user_router
 from .auth import router as auth_router
 
 app = FastAPI()
+
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=[
+		"http://localhost:3000",
+		"http://127.0.0.1:3000",
+	],
+	allow_credentials=True,
+	allow_methods=["*"],
+	allow_headers=["*"],
+)
 
 app.include_router(health_router)
 app.include_router(register_user_router)
